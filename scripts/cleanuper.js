30ff3d39d0cd4bef0821c4fff82cf89ae581b6c2
joker876 <joker876x@gmail.com>
2024-09-13 23:56:54 +0200

    upload starting icons

---
+++ scripts/cleanuper.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+
+const path = "../raw/";
+const fileNames = fs.readdirSync(path).filter((name) => name.endsWith(".svg"));
+
+let changedAmount = 0;
+for (const fileName of fileNames) {
+  let content = fs.readFileSync(path + fileName, "utf-8");
+  const startContent = content;
+
+  const foundComments = content.match(/<!--.+?-->/gs);
+  if (foundComments) {
+    content = content.replace(/<!--.+?-->\n?/gs, "");
+  }
+
+  const foundXml = content.match(/<\?xml.+?\?>/gs);
+  if (foundXml) {
+    content = content.replace(/<\?xml.+?\?>\n*/g, "");
+  }
+
+  const foundSt1s = content.match(/\.st[1-9]/g);
+  if (foundSt1s) {
+    content = content.replace(/\s*\.st[1-9]\{.+?\}\s*/g, "");
+    content = content.replace(/class="st[1-9]"/g, "class=\"st0\"");
+  }
+
+  const foundNewlines = content.match(/\u000d\n(\u000d\n)+/g);
+  if (foundNewlines) {
+    content = content.replace(/\u000d\n(\u000d\n)+/g, "");
+  }
+  if (startContent !== content) {
+
+    fs.writeFileSync(path + fileName, content, { encoding: "utf-8" });
+    changedAmount++; 
+  }
+}
+
+if (changedAmount) {
+  console.log(`Finished cleaning up ${fileNames.length} files (changed ${changedAmount})`);
+} else {
+  console.log('All files were clean. Nothing to do.');
+}
---
+++ scripts/finder.js
@@ -0,0 +1,81 @@
+const fs = require("fs");
+
+const path = "../raw/";
+const fileNames = fs
+  .readdirSync(path)
+  .filter((name) => name.endsWith(".svg"));
+
+const oldFileNames = [];
+for (const fileName of fileNames) {
+  if (fileName.match(/^[A-Z]/) && !fileName.match(/TEMPLATE/)) {
+    oldFileNames.push(fileName);
+  }
+}
+fs.writeFileSync("names.json", JSON.stringify(oldFileNames, null, 2));
+
+let logged = false;
+const comments = {};
+const titles = {};
+const ids = {};
+const strokes = {};
+const fills = {};
+const displayNones = {};
+const st1s = {};
+for (const fileName of fileNames) {
+  const content = fs.readFileSync(path + fileName, "utf-8");
+
+  const foundComments = content.match(/<!--.+?-->/gs);
+  if (foundComments) {
+    comments[fileName] = foundComments;
+  }
+
+  const foundTitles = content.match(/<title>.+?<\/title>/gs);
+  if (foundTitles) {
+    titles[fileName] = foundTitles;
+  }
+
+  const foundIds = content
+    .match(/id=".+?"/g)
+    ?.map((v) => v.match(/id="(.+?)"/)[1])
+  if (foundIds?.length) {
+    ids[fileName] = foundIds;
+
+    const newContent = content.replace(/\s*id=".+?"/g, "");
+    fs.writeFileSync(path + fileName, newContent, { encoding: "utf-8" });
+  }
+
+  const foundStrokes = content
+    .match(/stroke-width=".+?"/g)
+    ?.map((v) => v.match(/stroke-width="(.+?)"/)[1])
+    .filter((v) => !v.match(/^1.5$/));
+  if (foundStrokes?.length) {
+    strokes[fileName] = foundStrokes;
+  }
+
+  const foundFills = content
+    .match(/fill=".+?"/g)
+    ?.map((v) => v.match(/fill="(.+?)"/)[1])
+    .filter((v) => !v.match(/^none$/));
+  if (foundFills?.length) {
+    fills[fileName] = foundFills;
+  }
+
+  const foundDisplayNones = content.match(/display:none/g);
+  if (foundDisplayNones?.length) {
+    displayNones[fileName] = foundDisplayNones;
+  }
+
+  const foundSt1s = content.match(/.st1/g);
+  if (foundSt1s?.length) {
+    st1s[fileName] = foundSt1s;
+  }
+}
+fs.writeFileSync("comments.json", JSON.stringify(comments, null, 2));
+fs.writeFileSync("titles.json", JSON.stringify(titles, null, 2));
+fs.writeFileSync("ids.json", JSON.stringify(ids, null, 2));
+fs.writeFileSync("strokes.json", JSON.stringify(strokes, null, 2));
+fs.writeFileSync("fills.json", JSON.stringify(fills, null, 2));
+fs.writeFileSync("displayNones.json", JSON.stringify(displayNones, null, 2));
+fs.writeFileSync("st1s.json", JSON.stringify(st1s, null, 2));
+
+console.log(`Finished analyzing ${fileNames.length} files`);
