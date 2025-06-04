a71da513a720ce9132ef20ef27eeda8736496e7a
joker876 <joker876x@gmail.com>
2025-06-04 10:25:52 +0200

    create icon-data-tool app

--- scripts/NODE-copy-to-data-tool.js
+++ scripts/NODE-copy-to-data-tool.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-fs.cpSync("../raw", "../projects/homepage/src/assets/icons", {
+fs.cpSync("../raw", "../projects/icon-data-tool/public/icons", {
   recursive: true,
   filter: (src) => {
     return src.endsWith(".svg");
