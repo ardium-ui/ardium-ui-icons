bf3c66502c30cb5a9ee62367e9d0f6abadbd0d67
joker876 <joker876x@gmail.com>
2025-06-12 16:13:39 +0200

    start assigning categories

--- projects/homepage/src/app/services/icon-storage/icon-data.ts
+++ projects/homepage/src/app/services/icon-storage/icon-data.ts
@@ -2,6 +2,22 @@ import { IconCategory } from '@components/category-selector/categories';
 import { IconData, IconType } from '@services/icon-storage/icon-storage.types';
 
 export const ICON_DATA: IconData[] = [
+  { name: 'alert', type: IconType.Outlined, category: IconCategory.Alerts, tags: [] },
+  { name: 'alert-check', type: IconType.Outlined, category: IconCategory.Alerts, tags: [] },
+  { name: 'alert-error', type: IconType.Outlined, category: IconCategory.Alerts, tags: [] },
+  { name: 'alert-heart', type: IconType.Outlined, category: IconCategory.Alerts, tags: [] },
+  { name: 'alert-info', type: IconType.Outlined, category: IconCategory.Alerts, tags: [] },
+  { name: 'alert-minus', type: IconType.Outlined, category: IconCategory.Alerts, tags: [] },
+  { name: 'alert-not-allowed', type: IconType.Outlined, category: IconCategory.Alerts, tags: [] },
+  { name: 'alert-off', type: IconType.Outlined, category: IconCategory.Alerts, tags: [] },
+  { name: 'alert-plus', type: IconType.Outlined, category: IconCategory.Alerts, tags: [] },
+  { name: 'alert-question', type: IconType.Outlined, category: IconCategory.Alerts, tags: [] },
+  { name: 'alert-refresh', type: IconType.Outlined, category: IconCategory.Alerts, tags: [] },
+  { name: 'alert-reload', type: IconType.Outlined, category: IconCategory.Alerts, tags: [] },
+  { name: 'alert-reload-2', type: IconType.Outlined, category: IconCategory.Alerts, tags: [] },
+  { name: 'alert-search', type: IconType.Outlined, category: IconCategory.Alerts, tags: [] },
+  { name: 'alert-star', type: IconType.Outlined, category: IconCategory.Alerts, tags: [] },
+  { name: 'alert-x', type: IconType.Outlined, category: IconCategory.Alerts, tags: [] },
   { name: '3g-circle', type: IconType.Outlined, category: IconCategory.Animals, tags: [] },
   { name: '3g-square', type: IconType.Outlined, category: IconCategory.Animals, tags: [] },
   { name: '4g-circle', type: IconType.Outlined, category: IconCategory.Other, tags: ['network'] },
